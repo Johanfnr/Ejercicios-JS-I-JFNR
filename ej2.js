c2e51767a3977493e60762a5a08ab661eb3b1b73
Johan Nino <johanfnr@gmail.com>
2023-03-09 21:03:38 +0100

    EJERCICIO 2

--- ej2.js
+++ ej2.js
@@ -1,6 +1,3 @@
-//Este codigo me permite mostrar un mensaje en la consola
-console.log("Hola Mundo!");
-
 let mensaje = "Hola Mundo! \nQue facil es incluir 'comillas simples' \ny \"comillas dobles\""
 console.log(mensaje);
 alert(mensaje)
